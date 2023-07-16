9c3a82a755b9b970f217a1e9af78cd63baa328a1
E3ge0 <pollet.camille1407@gmail.com>
2023-07-16 18:20:16 +0200

    t

--- inc/bdd.php
+++ inc/bdd.php
@@ -1,5 +1,5 @@
 <?php
-$bdd = new PDO('mysql:host=127.0.0.1;dbname=u117330489_shaidy', 'u117330489_shaidy', '#vmdQIJ0');
+$bdd = new PDO('mysql:host=89.117.169.204;dbname=u117330489_shaidy', 'u117330489_shaidy', '#vmdQIJ0');
 if (!$bdd) {
     echo 'Connexion échouée';
 }
